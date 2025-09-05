3fb6a8244e57bab09856ad44452f21763d2ad25e
Rocco Totaro <cinema.totaro@gmail.com>
2025-09-05 20:59:08 +0200

    Delete pages/api/logout.js

--- pages/api/logout.js
+++
@@ -1,6 +0,0 @@
-import { clearAuthCookie } from "../../lib/auth";
-export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  clearAuthCookie(res);
-  res.redirect(302, '/');
-}
